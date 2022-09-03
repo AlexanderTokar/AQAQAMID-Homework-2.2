d1210070dbc02096abdc28ad8e92f7f35255c045
AlexanderTokar <alextokar@inbox.ru>
2022-09-03 14:06:57 +0300

    Fixed mistake at PostmanEchoTest.java to passed test

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("PostmanEchoPostTestFailed"))
+                .body("data", equalTo("PostmanEchoPostTest"))
         ;
     }
 }
