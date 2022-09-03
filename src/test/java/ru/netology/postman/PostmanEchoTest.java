4a04fb178ce7c0e3d5b048923de9f7c34ef1c485
AlexanderTokar <alextokar@inbox.ru>
2022-09-03 14:05:27 +0300

    Made mistake at PostmanEchoTest.java to failed test

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -3,6 +3,7 @@ package ru.netology.postman;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
 
@@ -12,14 +13,14 @@ public class PostmanEchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("PostmanEchoPostTest") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                //.body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("PostmanEchoPostTestFailed"))
         ;
     }
 }
