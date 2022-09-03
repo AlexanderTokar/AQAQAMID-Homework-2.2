176703c8833de62af65da6aec5deb080f0ab264f
AlexanderTokar <alextokar@inbox.ru>
2022-09-03 13:57:13 +0300

    empty test file added

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldReturnPostRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                //.body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
